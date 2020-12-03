fdd468fd1138e208472115ebec1f7d593d0cdce6
canvantuan <canvantuan12@gmail.com>
2020-12-03 13:39:59 +0700

    test app

---
+++ pages/test1.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Test2() {
+	return (
+		<div>
+			<Head>
+				<title>Can Van Tuan Test App 11</title>
+				<meta property="og:title" content="Can Van Tuan Test App 11" key="title" />
+				<meta name="description" content="Can Van Tuan Test App 11" />
+				<meta name="googlebot" content="Can Van Tuan Test App 11" />
+			</Head>
+			<div>Can Van Tuan Test App 11</div>
+		</div>
+	)
+}
---
+++ pages/test2.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Test2() {
+	return (
+		<div>
+			<Head>
+				<title>Can Van Tuan Test App 2</title>
+				<meta property="og:title" content="Can Van Tuan Test App 2" key="title" />
+				<meta name="description" content="Can Van Tuan Test App 2" />
+				<meta name="googlebot" content="Can Van Tuan Test App 2" />
+			</Head>
+			<div>Can Van Tuan Test App 2</div>
+		</div>
+	)
+}
